86c7356be4c2c79b56c17e13d273b45a466e832f
Simon Kovtyk <simon@kovtyk.com>
2025-06-13 13:31:05 +0200

    chore: upgraded dependencies

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 export default {
   "*.{ts,js,mjs,cjs}": "eslint",
-  "package.json": "npmPkgJsonLint -c ./node_modules/@ogs/linter/package-json.rules.json"
+  "package.json": "npmPkgJsonLint -c ./node_modules/@ogs-gmbh/linter/package-json-open-source.rules.json"
 };
 
