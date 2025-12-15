1103097956aca5721a2507185f196457a4ed20a2
Ian Wenneckers <iwenneck@gmail.com>
2025-12-15 16:08:12 +0100

    fix(translation-pipe): add subject for reactive update of token

--- src/pipes/translation.pipe.ts
+++ src/pipes/translation.pipe.ts
@@ -1,21 +1,41 @@
-import { ChangeDetectorRef, OnDestroy, Pipe, PipeTransform, inject } from "@angular/core";
+import { ChangeDetectorRef, DestroyRef, Pipe, PipeTransform, inject } from "@angular/core";
 import { SpecificTranslateConfig, TRANSLATION_CONFIG_TOKEN } from "../public-api";
-import { EMPTY, Observable, Subscription, catchError, filter } from "rxjs";
+import { EMPTY, Observable, Subject, catchError, distinctUntilChanged, of, switchMap } from "rxjs";
 import { TRANSLATION_SCOPE_TOKEN } from "../tokens/scope.token";
 import { TranslationService } from "../services/translation.service";
 import { resolveScope } from "../utils/translate.util";
 import { HttpErrorResponse } from "@angular/common/http";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { isEqual } from "es-toolkit";
+
+/**
+ * TODO: consider making public
+ * @internal
+ *
+ * Represents a localizable Translation
+ *
+ * @since 1.3.1
+ * @author Ian Wenneckers
+ *
+ */
+interface TranslationInfo {
+  defaultText: string;
+  token: string;
+  scope?: Readonly<string | Array<string | null> | null> | undefined;
+  shouldFallback?: boolean | undefined;
+}
+
 
 @Pipe({
   name: "translate",
   pure: false
 })
-export class TranslationPipe implements PipeTransform, OnDestroy {
-  private _translationServiceSubscription: Subscription | null = null;
+export class TranslationPipe implements PipeTransform {
+  private readonly _translation$: Subject<TranslationInfo> = new Subject<TranslationInfo>();
+
+  private readonly _destroyRef: DestroyRef = inject(DestroyRef);
 
   private _lastValue: string | null = null;
-
-  private _hasTranslationChanged: boolean = false;
 
   private readonly _translationService: TranslationService = inject(TranslationService);
 
@@ -24,6 +44,27 @@ export class TranslationPipe implements PipeTransform, OnDestroy {
   private readonly _translationScopeToken: string | null = inject(TRANSLATION_SCOPE_TOKEN, { optional: true });
 
   private readonly _translationConfig: SpecificTranslateConfig | null = inject(TRANSLATION_CONFIG_TOKEN, { optional: true });
+
+  constructor () {
+    this._translation$
+      .pipe(
+        takeUntilDestroyed(this._destroyRef),
+        distinctUntilChanged(isEqual),
+        switchMap((translationInfo: Readonly<TranslationInfo>) => this._translationService
+          .translateTokenByLocale$(translationInfo.token, translationInfo.defaultText, this._resolveScope(translationInfo.scope))
+          .pipe(
+            catchError((_httpErrorResponse: HttpErrorResponse, _: Observable<string>): Observable<string> => {
+              const fallbackToSourceLocale: boolean | undefined = translationInfo.shouldFallback ?? this._translationConfig?.fallbackToSourceLocale;
+
+              return fallbackToSourceLocale ? of(translationInfo.defaultText) : EMPTY;
+            }),
+            distinctUntilChanged()
+          ))
+      ).subscribe((translatedValue: string) => {
+        this._updateLastValue(translatedValue);
+      });
+  }
+
 
   /**
    * Transforms a token into its translated value
@@ -41,35 +82,18 @@ export class TranslationPipe implements PipeTransform, OnDestroy {
     value: string,
     token: string,
     scope?: Readonly<Array<string | null> | string | null>,
-    fallback?: boolean
+    shouldFallback?: boolean
   ): string {
-    if (this._translationServiceSubscription !== null)
-      return this._lastValue ?? value;
+    const translationInfo: TranslationInfo = {
+      defaultText: value,
+      token,
+      scope,
+      shouldFallback
+    };
 
-    this._translationServiceSubscription = this._translationService.translateTokenByLocale$(token, value, this._resolveScope(scope))
-      .pipe(
-        catchError((_httpErrorResponse: HttpErrorResponse, _: Observable<string>): Observable<string> => {
-          const fallbackToSourceLocale: boolean | undefined = fallback ?? this._translationConfig?.fallbackToSourceLocale;
-
-          if (fallbackToSourceLocale)
-            this._updateLastValue(value);
-
-          return EMPTY;
-        }),
-        filter((translation: string): boolean => translation !== this._lastValue)
-      )
-      .subscribe({
-        next: (translation: string): void => {
-          this._hasTranslationChanged = translation !== this._lastValue;
-          this._updateLastValue(translation);
-        }
-      });
+    this._translation$.next(translationInfo);
 
     return this._lastValue ?? value;
-  }
-
-  public ngOnDestroy (): void {
-    this._translationServiceSubscription?.unsubscribe();
   }
 
   private _resolveScope<T extends ReadonlyArray<string | null> | string | null>(scope?: T): T {
@@ -77,7 +101,9 @@ export class TranslationPipe implements PipeTransform, OnDestroy {
   }
 
   private _updateLastValue (newValue: string): void {
+    if (this._lastValue === newValue) return;
+
     this._lastValue = newValue;
-    this._hasTranslationChanged && this._changeDetectorRef.markForCheck();
+    this._changeDetectorRef.markForCheck();
   }
 }
